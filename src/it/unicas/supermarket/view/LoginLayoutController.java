647493e45c08ebf265fd016616552b65bc4de505
Giulio Russo <giulio@elioecaterina.it>
2021-11-29 11:13:25 +0100

    29-11-2021 GR torna ai reparti e finestra scontrino

--- src/it/unicas/supermarket/view/LoginLayoutController.java
+++ src/it/unicas/supermarket/view/LoginLayoutController.java
@@ -29,5 +29,4 @@ public class LoginLayoutController {
         App.getInstance().handleValidate();
 
     }
-
 }
--- src/it/unicas/supermarket/view/OrderSummaryLayoutController.java
+++ src/it/unicas/supermarket/view/OrderSummaryLayoutController.java
@@ -1,4 +1,18 @@
 package it.unicas.supermarket.view;
 
+import it.unicas.supermarket.App;
+import javafx.fxml.FXML;
+
 public class OrderSummaryLayoutController {
+
+    @FXML
+    private void handleMarket(){
+        App.getInstance().initMarketSectionLayout();
+    }
+
+    @FXML
+    private void handleShowPayment() {
+        System.out.println("Going to the Receipt section ...");
+        App.getInstance().showReceipt();
+    }
 }
